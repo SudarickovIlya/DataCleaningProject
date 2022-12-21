55a0e4b9fcc8ed89a86ef32c9ff0c21c4411a79d
SudarickovIlya <119815506+SudarickovIlya@users.noreply.github.com>
2022-12-22 00:17:16 +0300

    Add comment to the lesson

--- Библиотека_NumPy/Ordereddict.py
+++ Библиотека_NumPy/Ordereddict.py
@@ -17,3 +17,17 @@ ordered_client_ages['Sonya'] = 24
 del ordered_client_ages['Sonya']
 print(ordered_client_ages)
 print()
+
+print(4)
+from collections import defaultdict
+ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foods', 4.8),
+           ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
+           ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
+
+cafe=defaultdict(float)
+for name, score in ratings:
+    cafe[name]=score
+    
+cafes = OrderedDict(sorted(ratings, key=lambda x: (-x[1], x[0])))
+# Обратная сортировка по рейтингу, прямая по названию (алфавитная)
+print(cafes)
